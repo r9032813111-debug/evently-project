e865526c1f00b52dbbbce69664a8d4470211ca07
Roman <r9032813111@gmail.com>
2026-05-13 21:54:40 +0300

    Improve run.py for Render compatibility

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
+import sys
 from app import create_app
+
+# Важно для Render
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
